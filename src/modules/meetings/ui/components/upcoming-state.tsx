eb753cdbc59e4be2267d373729e6740fafe8cb38
adi2405 <144616497+adi2405@users.noreply.github.com>
2025-12-05 23:47:39 +0530

    21: meeting variants

---
+++ src/modules/meetings/ui/components/upcoming-state.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import { VideoIcon, BanIcon } from "lucide-react";
+
+import { EmptyState } from "@/components/custom/empty-state";
+import { Button } from "@/components/ui/button";
+
+interface Props {
+  meetingId: string;
+  onCancelMeeting: () => void;
+  isCancelling: boolean;
+}
+
+export const UpcomingState = ({
+  meetingId,
+  onCancelMeeting,
+  isCancelling,
+}: Props) => {
+  return (
+    <div className="bg-white rounded-lg px-4 py-5 flex flex-col gap-y-8 items-center justify-center">
+      <EmptyState
+        image="/upcoming.svg"
+        title="Meeting not started yet"
+        description="Once you start this meeting, a summary will appear hear"
+      />
+      <div className="flex flex-col-reverse lg:flex-row lg:justify-center items-center gap-2">
+        <Button
+          variant={"secondary"}
+          className="w-full lg:w-auto"
+          onClick={onCancelMeeting}
+          disabled={isCancelling}
+        >
+          <BanIcon />
+          Cancel meeting
+        </Button>
+        <Button asChild disabled={isCancelling} className="w-full lg:w-auto">
+          <Link href={`/call/${meetingId}`}>
+            <VideoIcon />
+            Start meeting
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
